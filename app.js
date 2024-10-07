c92c7cc6ccd76f99391f30c732ae87988231c2f1
Kirti-Eppanapelli <d2021.kirti.eppanapelli@ves.ac.in>
2024-10-07 23:52:53 +0530

    added

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ window.onscroll = () => {
     else{document.querySelector('header').classList.remove('active');}
 }
 
-/*themeToggler.onclick = function() {
+themeToggler.onclick = function() {
     document.body.classList.toggle('dark-theme');
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
-}*/
+}
 
 let setData = (day) =>{
     document.querySelector('table tbody').innerHTML = ' '; //To clear out previous table data;  
